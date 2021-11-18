119c0aad92febe117d6fe58a7327b080ff45ca36
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-11-18 17:57:27 -0300

    Foram adicionados os métodos saca, deposita
    e transfere.

--- bytebank/Conta.java
+++ bytebank/Conta.java
@@ -4,4 +4,33 @@ public class Conta{
     int agencia;
     int numero;
     String titular;
+    
+    
+    public void deposita (double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca (double valor) {
+		if (this.saldo>=valor) {
+			this.saldo -=valor;
+			return true;
+		}
+		else {
+			return false;
+					}
+		
+		}
+	
+	public boolean transfere (double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita (valor);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 }
+	
