8272dacf81d6f5b7fc827ea328f35ba8e80808d9
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-11-19 15:44:29 -0300

    Testando agora com o public static void main
    ok

--- bytebank/TestesCliente.java
+++ bytebank/TestesCliente.java
@@ -1,7 +1,14 @@
-
 public class TestesCliente {
+	
+	public static void main(String[] args) {
+	
+		Conta contaMarcelo = new Conta();
+		contaMarcelo.titular = new Cliente();
+		contaMarcelo.titular.nome = "Marcelo";
+		System.out.println(contaMarcelo.titular.nome);
+		System.out.println(contaMarcelo.titular.cpf);
+		System.out.println(contaMarcelo.titular.profissao);
 		
-	Conta contaMarcelo = new Conta();
-	contaMarcelo.titular = new Cliente();
-
+	}
+		
 }
